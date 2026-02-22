7c8678720b43c7c0f68834b91119a1aaef4e4b45
Jerry-Shen0527 <jerry_shen0527@outlook.com>
2026-02-22 12:56:49 +0800

    Fix warning

--- source/Plugins/hd_RUZINO_GL/api.h
+++ source/Plugins/hd_RUZINO_GL/api.h
@@ -1,8 +1,12 @@
 
 #pragma once
 
-#define RUZINO_NAMESPACE_OPEN_SCOPE  namespace Ruzino {
+#ifndef RUZINO_NAMESPACE_OPEN_SCOPE
+#define RUZINO_NAMESPACE_OPEN_SCOPE namespace Ruzino {
+#endif
+#ifndef RUZINO_NAMESPACE_CLOSE_SCOPE
 #define RUZINO_NAMESPACE_CLOSE_SCOPE }
+#endif
 
 #if defined(_MSC_VER)
 #define HD_RUZINO_GL_EXPORT   __declspec(dllexport)
